2c5f599f78ac42e8b64832ce955faf2c26d1502e
hbl <1372263592@qq.com>
2022-01-13 14:36:28 +0800

    统一返回结果。 R:{"success":true,"code":20000,"message":"成功","data":{}}

---
+++ common/common-utils/src/main/java/com/hbl/commonutils/ResultCode.java
@@ -0,0 +1,8 @@
+package com.hbl.commonutils;
+
+public interface ResultCode {
+	//状态码：成功
+	Integer SUCCESS = 20000;
+	//状态码：失败
+	Integer ERROR = 20001;
+}
--- service/service-edu/src/main/java/com/hbl/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/hbl/eduservice/controller/EduTeacherController.java
@@ -1,6 +1,7 @@
 package com.hbl.eduservice.controller;
 
 
+import com.hbl.commonutils.R;
 import com.hbl.eduservice.entity.EduTeacher;
 import com.hbl.eduservice.service.EduTeacherService;
 import io.swagger.annotations.ApiOperation;
@@ -29,15 +30,21 @@ public class EduTeacherController {
 	//查询讲师表所有数据
 	@GetMapping("/findAll")
 	@ApiOperation("所有讲师列表")
-	public List<EduTeacher> list() {
-		return eduTeacherService.list(null);
+	public R list() {
+		List<EduTeacher> list = eduTeacherService.list(null);
+		return R.ok().data("items",list);
 	}
 
 	//逻辑删除讲师
 	@ApiOperation(value = "根据ID删除讲师")
 	@DeleteMapping("/deleteTeacherById/{id}")
-	public boolean deleteTeacherById(@ApiParam(name = "id", value = "讲师Id", required = true) @PathVariable String id) {
-		return eduTeacherService.removeById(id);
+	public R deleteTeacherById(@ApiParam(name = "id", value = "讲师Id", required = true) @PathVariable String id) {
+		boolean flag = eduTeacherService.removeById(id);
+		if (flag) {
+			return R.ok();
+		} else {
+			return R.error();
+		}
 	}
 
 
