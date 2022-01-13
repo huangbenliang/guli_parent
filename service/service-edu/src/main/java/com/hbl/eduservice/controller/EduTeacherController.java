d228b044e35c6404ce69fff7b6c0abb1422def8a
hbl <1372263592@qq.com>
2022-01-13 14:21:01 +0800

    完善swagger2，使得文档更加详细。

--- service/service-edu/src/main/java/com/hbl/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/hbl/eduservice/controller/EduTeacherController.java
@@ -3,6 +3,8 @@ package com.hbl.eduservice.controller;
 
 import com.hbl.eduservice.entity.EduTeacher;
 import com.hbl.eduservice.service.EduTeacherService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,18 +21,22 @@ import java.util.List;
 @RestController
 @CrossOrigin //解决跨域问题
 @RequestMapping("/eduservice/edu-teacher")
+//@Api(description = "讲师管理")
 public class EduTeacherController {
 	@Autowired
 	private EduTeacherService eduTeacherService;
 
 	//查询讲师表所有数据
 	@GetMapping("/findAll")
-	public List<EduTeacher> list(){
+	@ApiOperation("所有讲师列表")
+	public List<EduTeacher> list() {
 		return eduTeacherService.list(null);
 	}
+
 	//逻辑删除讲师
+	@ApiOperation(value = "根据ID删除讲师")
 	@DeleteMapping("/deleteTeacherById/{id}")
-	public boolean deleteTeacherById(@PathVariable String id){
+	public boolean deleteTeacherById(@ApiParam(name = "id", value = "讲师Id", required = true) @PathVariable String id) {
 		return eduTeacherService.removeById(id);
 	}
 
